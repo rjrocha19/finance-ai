1c27acb23ae504868ffd3277986181565ae7ebb7
rjrocha19 <rodolfo_rj@live.com>
2024-11-09 12:59:56 -0300

    chore: update Add Transaction Button

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -1,11 +1,40 @@
 import { ArrowDownUpIcon } from 'lucide-react';
 import { Button } from './ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
 
 export default function AddTransactionButton() {
   return (
-    <Button className="rounded-full font-bold">
-      Adicionar Transação
-      <ArrowDownUpIcon />
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="rounded-full font-bold">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar Transação</DialogTitle>
+          <DialogDescription className="text-pretty">
+            Insira as informações abaixo
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button variant="outline" className="w-full rounded-xl">
+            Cancelar
+          </Button>
+          <Button variant="default" className="w-full rounded-xl">
+            Adicionar
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
