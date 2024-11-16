95ff4d7c0d5826b618bb0a4fb6fb718ab65e5d74
rjrocha19 <rodolfo_rj@live.com>
2024-11-16 08:01:22 -0300

    chore: update project

--- app/_actions/add-transaction/index.ts
+++ app/_actions/add-transaction/index.ts
@@ -2,16 +2,29 @@
 
 import { db } from '@/app/_lib/prisma';
 import { auth } from '@clerk/nextjs/server';
-import type { Prisma } from '@prisma/client';
+import type {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from '@prisma/client';
 import { addTransactionSchema } from '../schema';
+import { revalidatePath } from 'next/cache';
 
-export const addTransaction = async (
-  params: Omit<Prisma.TransactionCreateInput, 'userId'>,
-) => {
+interface AddTransactionParams {
+  name: string;
+  amount: number;
+  type: TransactionType;
+  category: TransactionCategory;
+  paymentMethod: TransactionPaymentMethod;
+  date: Date;
+}
+
+export const addTransaction = async (params: AddTransactionParams) => {
   addTransactionSchema.parse(params);
   const { userId } = await auth();
   if (!userId) {
     throw new Error('Unauthorized');
   }
   await db.transaction.create({ data: { ...params, userId } });
+  revalidatePath('/transactions');
 };
